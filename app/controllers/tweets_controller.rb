4861af701048bc8af21a7ae37bb3153220db99c5
toshiomi <19881005mnmtsom@gmail.com>
2019-12-09 18:13:41 +0900

    ルーティング、コントローラ、モデル、仮ビュー作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,9 @@
 class TweetsController < ApplicationController
 
   def index
+    @tweets = Tweet.all
   end
-  
+
   def new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :tweets
-  # get 'tweets' => 'tweets#index'
+  root 'tweets#index'
 end
