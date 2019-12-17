1f6b0efe55fc57b508a61d4a2b9dcdc5dc704e58
toshiomi <19881005mnmtsom@gmail.com>
2019-12-17 23:54:52 +0900

    コメントの非同期通信

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,30 @@
+$(function () {
+  function buildHTML(comment) {
+    var html = `<p>
+                  <strong>
+                    <a href=/users/${comment.user_id}>${comment.user_name}</a>
+                    ：
+                  </strong>
+                  ${comment.text}
+                </p>`
+    return html;
+  }
+  $('#new_comment').on('submit', function (e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+      .done(function (data) {
+        var html = buildHTML(data);
+        $('.comment_box').val('');
+        $('.form_submit').prop('disabled', false);
+      })
+  })
+})
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,9 @@
 class CommentsController < ApplicationController
   def create
-    Comment.create(tweet_id: comment_params[:tweet_id],user_id:current_user.id,text: comment_params[:text])
-    redirect_back(fallback_location: root_path)
+    @comment = Comment.create(tweet_id: comment_params[:tweet_id],user_id:current_user.id,text: comment_params[:text])
+    respond_to do |f|
+      format.html {redirect_back(fallback_location: root_path)}
+      format.json
   end
 
   private
