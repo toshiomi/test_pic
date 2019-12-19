01a0631893e4d24b3b624c2599f8ab4d09b417c9
toshiomi <19881005mnmtsom@gmail.com>
2019-12-19 21:56:46 +0900

    検索が成功した時と失敗した時の分岐、失敗時の処理

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,4 +1,13 @@
 $(function () {
+  var search_list = $(".search_field");
+
+  function searchTweet(tweet) {
+  }
+
+  function searchError(message) {
+    var html = `<div class='name'>${message}</div>`
+    search_list.append(html);
+  }
   $(".search_field").on("keyup", function () {
     var input = $(".search_field").val();
     $.ajax({
@@ -8,7 +17,15 @@ $(function () {
       dataType: "json"
     })
       .done(function (tweets) {
-
+        search_list.empty();
+        if (tweets.length !== 0) {
+          tweets.forEach(function (tweet) {
+            searchTweet(tweet);
+          });
+        }
+        else {
+          searchError("検索結果がありません")
+        }
       })
   });
 });
